8c9884e6178b5ea370cd3709504db24c00caca42
2goni <gh2684@naver.com>
2021-01-28 22:11:15 +0900

    Answer

---
+++ Q4/Q4.java
@@ -0,0 +1,14 @@
+
+public class Q4 {
+
+	public static void main(String[] args) {
+		int fahrenheit = 100;
+		float celcius = ((float)5/9)*(fahrenheit-32);
+		/* ¹®Á¦) È­¾¾¸¦ ¼·¾¾·Î º¯°æÇÏ´Â ÇÁ·Î±×·¥À» ÀÛ¼º ÇÏ½Ã¿À.
+		C(celcius): ¼·¾¾, F(fahrenheit): È­¾¾
+		°ø½Ä: C = 5/9 * (F-32) */
+		
+		System.out.println("Fahrenheit:"+fahrenheit);
+		System.out.println("Celcius:"+celcius);
+	}
+}
