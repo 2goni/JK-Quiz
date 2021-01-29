ecf57b1003fc47a772b88ee7d8bc613d8b242e07
2goni <gh2684@naver.com>
2021-01-29 15:26:49 +0900

    Answer

---
+++ Q6/Q6.java
@@ -0,0 +1,16 @@
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		
+		int i = 5;
+		String result = (i/2 == 0) ? "짝수" : "홀수";
+		/* 문제) 선언된 정수형 데이터가 홀수인지 짝수인지 출력한다. 
+		   단, 연산처리는 삼항 연산자를 이용한다.
+		 */
+		
+		System.out.println(result);
+		
+	}
+
+}
